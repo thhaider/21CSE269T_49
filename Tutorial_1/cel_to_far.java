0cf20466b6583a4532b0f5400400b2a45be1a7bf
Haider Shah <113873489+thhaider@users.noreply.github.com>
2025-02-02 14:16:07 +0530

    Create cel_to_far.java

---
+++ Tutorial_1/cel_to_far.java
@@ -0,0 +1,13 @@
+// c to f
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+        scanner.close();
+    }
+}
