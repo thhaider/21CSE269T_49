d5d7fd2ba3dec9be6b832aa982730f44f8de6c94
Haider Shah <113873489+thhaider@users.noreply.github.com>
2025-02-02 13:59:25 +0530

    Create add.java

---
+++ Tutorial_1/add.java
@@ -0,0 +1,14 @@
+// addition of 2 numbers 
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+        scanner.close();
+    }
